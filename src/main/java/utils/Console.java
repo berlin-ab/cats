474bd89438fc7c58bedfd27ff0b31d11cf0faa24
Adam Berlin and Vanessa Sant'Anna <pair+aberlin+vsantanna@pivotallabs.com>
2014-05-15 16:22:16 -0400

    Switched to using a spring application builder.

--- src/main/java/utils/Console.java
+++ src/main/java/utils/Console.java
@@ -1,10 +1,11 @@
-package cats;
+package utils;
 
 import groovy.lang.Binding;
 import org.codehaus.groovy.tools.shell.Groovysh;
 import org.codehaus.groovy.tools.shell.IO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +15,15 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration
 public class Console {
     public static void main(String[] args) {
-        ApplicationContext context = SpringApplication.run(Console.class, args);
+        ApplicationContext context = new SpringApplicationBuilder()
+                .main(Console.class)
+                .sources(Console.class)
+                .showBanner(false)
+                .headless(true)
+                .web(false)
+                .run();
+
+
         Binding binding = new Binding();
         binding.setProperty("app", context);
         new Groovysh(binding, new IO()).run((String) null);
