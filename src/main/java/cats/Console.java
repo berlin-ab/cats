e2267f7334c4e4626ef0436c04d3bd0d8477bca3
Adam Berlin and Vanessa Sant'Anna <pair+aberlin+vsantanna@pivotallabs.com>
2014-05-15 15:59:02 -0400

    Proof of concept REPL.

--- src/main/java/cats/Console.java
+++ src/main/java/cats/Console.java
@@ -3,13 +3,20 @@ package cats;
 import groovy.lang.Binding;
 import org.codehaus.groovy.tools.shell.Groovysh;
 import org.codehaus.groovy.tools.shell.IO;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-/**
- * Created by pivotal on 5/15/14.
- */
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
 public class Console {
     public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(Console.class, args);
         Binding binding = new Binding();
-        new Groovysh(binding, new IO());
+        binding.setProperty("app", context);
+        new Groovysh(binding, new IO()).run((String) null);
     }
 }
